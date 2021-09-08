6f04821e429b23ed57ab2d3bbd6e8a70e6c4fe0a
xZenshin <askoularsen@gmail.com>
2021-09-08 13:59:16 +0200

    Tested and implemented IsLeapYear

--- Assignment0.Tests/Program.cs
+++ Assignment0.Tests/Program.cs
@@ -1,1 +1,24 @@
-System.Console.WriteLine("Hello World");
+
+namespace Assignment0 
+    {
+
+public class TestClass 
+    {
+
+    public bool IsLeapYear(int year) 
+        {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                }
+                return false;
+            }
+            if (year % 4 == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
--- Assignment0.Tests/UnitTest1.cs
+++ Assignment0.Tests/UnitTest1.cs
@@ -6,8 +6,30 @@ namespace Assignment0.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void DivisibleByFour()
         {
+            var number = 40;
+            var testClass = new TestClass();
+            bool actual = testClass.IsLeapYear(number);
+            Assert.Equal(true, actual);
+        }
+
+        [Fact]
+        public void DivisibleByHundred() 
+        {
+            var number = 100;
+            var testClass = new TestClass();
+            bool actual = testClass.IsLeapYear(number);
+            Assert.Equal(false, actual);
+
+        }
+        [Fact]
+        public void DivisibleByFourHundred() 
+        {
+            var number = 400;
+            var testClass = new TestClass();
+            bool actual = testClass.IsLeapYear(number);
+            Assert.Equal(true, actual);
 
         }
     }
